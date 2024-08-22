90658344211d50fb07089dd48140b1b6123fd288
Dione V. Silva <135567312+Dionepx@users.noreply.github.com>
2024-08-21 23:44:12 -0300

    Add files via upload

---
+++ array java/index.java
@@ -0,0 +1,9 @@
+public class Main{
+     public static void main(String[] args){
+         // Array
+         // tipo[] nomeVariavel;
+         String[] nomes = new String[]{
+                 "João", "Daniel", "Maria", "Alberto", "Gustavo"
+         };
+     }
+ }
